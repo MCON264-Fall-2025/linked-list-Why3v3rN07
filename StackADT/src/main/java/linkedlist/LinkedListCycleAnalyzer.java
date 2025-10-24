1fa4d73b6472a826f4178717a288ae2033b6371d
Why3v3rN07 <hfogarty@student.touro.edu>
2025-10-24 01:16:09 -0400

    implement detectCycleInfo method
    Using Floyd's tortoise and hare algorithm

--- StackADT/src/main/java/linkedlist/LinkedListCycleAnalyzer.java
+++ StackADT/src/main/java/linkedlist/LinkedListCycleAnalyzer.java
@@ -1,11 +1,35 @@
 package linkedlist;
 
-import org.apache.commons.lang3.NotImplementedException;
 import support.CycleInfo;
 import support.LLNode;
 
-public class LinkedListCycleAnalyzer<T> {
-    public static <T> CycleInfo detectCycleInfo(LLNode<T> head){
-        throw new NotImplementedException("TODO: implement this method");
+public class LinkedListCycleAnalyzer {
+    public static <T> CycleInfo detectCycleInfo(LLNode<T> head) {
+        LLNode<T> tortoise = head, hare = head;
+
+        //run around till t and h meet, or return -1 if hare hits null
+        do {
+            if (hare == null || hare.getLink() == null) return new CycleInfo(-1, 0);
+            tortoise = tortoise.getLink();
+            hare = hare.getLink().getLink();
+        } while (tortoise != hare);
+
+        //now they're at the same place, sent tortoise around till it meets hare again, to count cycle length
+        int cycleLength = 0;
+        do {
+            tortoise = tortoise.getLink();
+            cycleLength++;
+        } while (tortoise != hare);
+
+        //hare still at meeting point, send tortoise back to head and have them both move one space at a time,
+        //rounds it takes to  meet up should equal distance from head to start of cycle = index of cycle entry point
+        int entryIndex = 0;
+        tortoise = head;
+        while (tortoise != hare) {
+            tortoise = tortoise.getLink();
+            hare = hare.getLink();
+            entryIndex++;
+        }
+        return new CycleInfo(entryIndex,  cycleLength);
     }
 }
--- StackADT/src/test/java/linkedlist/DetectCycleInfoTest.java
+++ StackADT/src/test/java/linkedlist/DetectCycleInfoTest.java
@@ -1,8 +1,11 @@
 package linkedlist;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import support.CycleInfo;
 import support.LLNode;
+
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -20,13 +23,23 @@ class DetectCycleInfoTest  {
         return head;
     }
 
-    @Test void example1() {
+    @DisplayName("Contains cycle - odd")
+    @Test void oddCycle() {
         LLNode<Integer> head = build(new int[]{3,2,0,-4}, 1);
         CycleInfo info = LinkedListCycleAnalyzer.detectCycleInfo(head);
         assertEquals(1, info.entryIndex);
         assertEquals(3, info.cycleLength);
     }
 
+    @DisplayName("Contains cycle - even")
+    @Test void evenCycle() {
+        LLNode<Integer> head = build(new int[]{4,-8,0,-2}, 2);
+        CycleInfo info = LinkedListCycleAnalyzer.detectCycleInfo(head);
+        assertEquals(2, info.entryIndex);
+        assertEquals(2, info.cycleLength);
+    }
+
+    @DisplayName("Cycle from index 0")
     @Test void example2() {
         LLNode<Integer> head = build(new int[]{1,2}, 0);
         CycleInfo info = LinkedListCycleAnalyzer.detectCycleInfo(head);
@@ -34,6 +47,15 @@ class DetectCycleInfoTest  {
         assertEquals(2, info.cycleLength);
     }
 
+    @DisplayName("No cycle")
+    @Test void example4_noCycle() {
+        LLNode<Integer> head = build(new int[]{1,3,2,4}, -1);
+        CycleInfo info = LinkedListCycleAnalyzer.detectCycleInfo(head);
+        assertEquals(-1, info.entryIndex);
+        assertEquals(0, info.cycleLength);
+    }
+
+    @DisplayName("No cycle - single")
     @Test void example3_noCycle() {
         LLNode<Integer> head = build(new int[]{1}, -1);
         CycleInfo info = LinkedListCycleAnalyzer.detectCycleInfo(head);
@@ -41,10 +63,21 @@ class DetectCycleInfoTest  {
         assertEquals(0, info.cycleLength);
     }
 
+    @DisplayName("Cycle - single")
     @Test void selfLoop() {
         LLNode<Integer> head = build(new int[]{7}, 0);
         CycleInfo info = LinkedListCycleAnalyzer.detectCycleInfo(head);
         assertEquals(0, info.entryIndex);
         assertEquals(1, info.cycleLength);
+    }
+
+    @DisplayName("Cycle - single")
+    @Test void longList() {
+        int[] longList = new int[100000];
+        for (int i = 0; i < 100000; i++) { longList[i] = i; }
+        LLNode<Integer> head = build(longList, 50000);
+        CycleInfo info = LinkedListCycleAnalyzer.detectCycleInfo(head);
+        assertEquals(50000, info.entryIndex);
+        assertEquals(50000, info.cycleLength);
     }
 }
